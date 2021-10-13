ResurrectionRemix/android_frameworks_base

Navbartile: Change longpress intent fragment [1/2]
Signed-off-by: Varun Date <date.varun123@gmail.com>
Change-Id: I9a9f8ca5e26377fd169ceb31d82a5373033d150c

packages/SystemUI/src/com/android/systemui/qs/tiles/NavBarTile.java
@@ -38,7 +38,7 @@ public class NavBarTile extends QSTileImpl<BooleanState> {
     private final Icon mIcon = ResourceIcon.get(R.drawable.ic_qs_navbar);
 
     private static final Intent NAVBAR_SETTINGS =
-            new Intent("org.lineageos.lineageparts.BUTTON_SETTINGS");
+            new Intent("android.settings.RR_BUTTONS");
 
     private final SystemSetting mSetting;
 

ROM-EXTRAS/crdroid_frameworks_base

packages/SystemUI/src/com/android/systemui/qs/tiles/NavBarTile.java
@@ -38,7 +38,7 @@ public class NavBarTile extends QSTileImpl<BooleanState> {
     private final Icon mIcon = ResourceIcon.get(R.drawable.ic_qs_navbar);
 
     private static final Intent NAVBAR_SETTINGS =
-            new Intent("org.lineageos.lineageparts.BUTTON_SETTINGS");
+            new Intent("android.settings.NAVBAR_SETTINGS");
 
     private final SystemSetting mSetting;
 