ROM-FIXES/android_frameworks_base

GestureAnywhere: Increase gesture anywhere width to 40px [1/2]
Users keep on complaining that Geature Anywhere doesn't work but is basically most don't read (big surprise)
and don't know they can adjust the width.

Change-Id: Ibbd00c780aaaffb4b9c040cb47ebe4db7f611a6c

packages/SystemUI/src/com/android/systemui/chaos/lab/gestureanywhere/GestureAnywhereView.java
@@ -124,7 +124,7 @@ public class GestureAnywhereView extends TriggerOverlayView implements GestureOv
             }
 
             int width = Settings.System.getInt(
-                    resolver, Settings.System.GESTURE_ANYWHERE_TRIGGER_WIDTH, 10);
+                    resolver, Settings.System.GESTURE_ANYWHERE_TRIGGER_WIDTH, 40);
             if (mTriggerWidth != width)
                 setTriggerWidth(width);
             setTopPercentage(Settings.System.getInt(

ROM-EXTRAS/crdroid_frameworks_base

packages/SystemUI/src/com/android/systemui/chaos/lab/gestureanywhere/GestureAnywhereView.java
@@ -124,7 +124,7 @@ public class GestureAnywhereView extends TriggerOverlayView implements GestureOv
             }
 
             int width = LineageSettings.System.getInt(
-                    resolver, LineageSettings.System.GESTURE_ANYWHERE_TRIGGER_WIDTH, 10);
+                    resolver, LineageSettings.System.GESTURE_ANYWHERE_TRIGGER_WIDTH, 40);
             if (mTriggerWidth != width)
                 setTriggerWidth(width);
             setTopPercentage(LineageSettings.System.getInt(