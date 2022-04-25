AOSPA/android_frameworks_base-old

PixelPropsUtils: switch gms spoof to redfin as well
Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
Change-Id: I3602d87ab9773e2eeca98baf81d5a9fb646770d9

core/java/com/android/internal/util/PixelPropsUtils.java
@@ -34,15 +34,11 @@ public class PixelPropsUtils {
     public static final String PACKAGE_GMS = "com.google.android.gms";
 
     private static final Map<String, Object> propsToChange;
-    private static final Map<String, Object> propsToChangeP6;
     private static final Map<String, Object> propsToChangeP5;
     private static final Map<String, Object> propsToChangeP1;
     private static final Map<String, ArrayList<String>> propsToKeep;
     private static final String[] extraPackagesToChange = {
         "com.android.chrome"
-    };
-    private static final String[] packagesToChangeP6 = {
-        "com.google.android.gms"
     };
     private static final String[] packagesToChangeP1 = {
          "com.google.android.apps.photos"
@@ -72,11 +68,6 @@ public class PixelPropsUtils {
         propsToChangeP5.put("PRODUCT", "redfin");
         propsToChangeP5.put("MODEL", "Pixel 5");
         propsToChangeP5.put("FINGERPRINT", "google/redfin/redfin:12/SP2A.220305.012/8177914:user/release-keys");
-        propsToChangeP6 = new HashMap<>();
-        propsToChangeP6.put("DEVICE", "raven");
-        propsToChangeP6.put("PRODUCT", "raven");
-        propsToChangeP6.put("MODEL", "Pixel 6 Pro");
-        propsToChangeP6.put("FINGERPRINT", "google/raven/raven:12/SP2A.220305.013.A3/8229987:user/release-keys");
         propsToChangeP1 = new HashMap<>();
         propsToChangeP1.put("DEVICE", "marlin");
         propsToChangeP1.put("PRODUCT", "marlin");
@@ -94,9 +85,7 @@ public class PixelPropsUtils {
         if ((packageName.startsWith("com.google.") && !Arrays.asList(packagesToKeep).contains(packageName))
                 || Arrays.asList(extraPackagesToChange).contains(packageName)) {
             if (DEBUG) Log.d(TAG, "Defining props for: " + packageName);
-            if (Arrays.asList(packagesToChangeP6).contains(packageName)) {
-                propsToChange.putAll(propsToChangeP6);
-            } else if (Arrays.asList(packagesToChangeP1).contains(packageName)) {
+            if (Arrays.asList(packagesToChangeP1).contains(packageName)) {
                 propsToChange.putAll(propsToChangeP1);
             } else {
                 propsToChange.putAll(propsToChangeP5);

ROM-EXTRAS/crdroid_frameworks_base

core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
@@ -34,17 +34,12 @@ public class PixelPropsUtils {
 
     private static final Map<String, Object> propsToChange;
     private static final Map<String, Object> propsToChangePixel5;
-    private static final Map<String, Object> propsToChangePixel6;
     private static final Map<String, Object> propsToChangePixelXL;
     private static final Map<String, ArrayList<String>> propsToKeep;
 
     private static final String[] extraPackagesToChange = {
             "com.android.chrome",
             "com.breel.wallpapers20"
-    };
-
-    private static final String[] packagesToChangePixel6 = {
-            "com.google.android.gms"
     };
 
     private static final String[] packagesToChangePixelXL = {
@@ -118,13 +113,6 @@ public class PixelPropsUtils {
         propsToKeep = new HashMap<>();
         propsToChange = new HashMap<>();
         propsToKeep.put("com.google.android.settings.intelligence", new ArrayList<>(Collections.singletonList("FINGERPRINT")));
-        propsToChangePixel6 = new HashMap<>();
-        propsToChangePixel6.put("BRAND", "google");
-        propsToChangePixel6.put("MANUFACTURER", "Google");
-        propsToChangePixel6.put("DEVICE", "raven");
-        propsToChangePixel6.put("PRODUCT", "raven");
-        propsToChangePixel6.put("MODEL", "Pixel 6 Pro");
-        propsToChangePixel6.put("FINGERPRINT", "google/raven/raven:12/SP2A.220405.004/8233519:user/release-keys");
         propsToChangePixel5 = new HashMap<>();
         propsToChangePixel5.put("BRAND", "google");
         propsToChangePixel5.put("MANUFACTURER", "Google");
@@ -159,9 +147,7 @@ public class PixelPropsUtils {
         if (packageName.startsWith("com.google.")
                 || Arrays.asList(extraPackagesToChange).contains(packageName)) {
 
-            if (Arrays.asList(packagesToChangePixel6).contains(packageName)) {
-                propsToChange.putAll(propsToChangePixel6);
-            } else if (Arrays.asList(packagesToChangePixelXL).contains(packageName)) {
+            if (Arrays.asList(packagesToChangePixelXL).contains(packageName)) {
                 propsToChange.putAll(propsToChangePixelXL);
             } else {
                 propsToChange.putAll(propsToChangePixel5);