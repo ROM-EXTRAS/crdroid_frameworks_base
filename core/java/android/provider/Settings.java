5274cb18679b52156ae554795f72fbb2415c044a
DroidFreak32 <rushabshah32@gmail.com>
2022-04-30 17:48:52 +0530

    Fixup

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5905,13 +5905,6 @@ public final class Settings {
         }
 
         /**
-         * Control whether FLAG_SECURE is ignored for all windows.
-         * @hide
-         */
-        @Readable
-        public static final String WINDOW_IGNORE_SECURE = "window_ignore_secure";
-
-        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
@@ -6884,6 +6877,12 @@ public final class Settings {
                 int userHandle) {
             return putStringForUser(cr, name, Float.toString(value), userHandle);
         }
+
+        /**
+         * Control whether FLAG_SECURE is ignored for all windows.
+         * @hide
+         */
+        public static final String WINDOW_IGNORE_SECURE = "window_ignore_secure";
 
         /**
          * Control whether to enable adaptive sleep mode.
